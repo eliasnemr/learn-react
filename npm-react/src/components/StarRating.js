a2d380b0da279740788dcd46d08fe4e215e35e7c
Elias <eliasnemr@gmail.com>
2025-04-28 12:18:30 +0300

    Added style property, and spread any other properties that may be added to the component

--- npm-react/src/components/StarRating.js
+++ npm-react/src/components/StarRating.js
@@ -5,10 +5,10 @@ const Star = ({ selected = false, onSelect = f => f }) => (<FaStar color={select
 
 const createArray = length => [...Array(length)];
 
-export default function StarRating({ totalStars = 5 }) {
+export default function StarRating({ style = {}, totalStars = 5, ...props }) {
     const [selectedStars, setSelectedStars] = useState(3);
 
-    return <>
+    return <div style={{ padding: "5px", ...style }} {...props}>
         {createArray(totalStars).map((n, i) => (
             <Star key={i} selected={selectedStars > i} onSelect={() => setSelectedStars(i + 1)} />
         ))}
@@ -16,5 +16,5 @@ export default function StarRating({ totalStars = 5 }) {
         <p>
             {selectedStars} of {totalStars} stars
         </p>
-    </>
+    </div>
 }
