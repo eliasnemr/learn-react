5d6637bcc5d556064599ba375f9c30d8ea0ca800
Elias <eliasnemr@gmail.com>
2025-04-29 08:58:35 +0300

    created a useInput hook to remove redundancy and copy/pasting

--- color-organizer/src/components/AddColorForm.js
+++ color-organizer/src/components/AddColorForm.js
@@ -1,22 +1,44 @@
-import React, { useRef } from "react";
+import React, { useState } from "react";
+import { useInput } from "../hooks/useInput";
 
-// imperative code, uncontrolled component
+// declarative code, controlled component, it is controlled by react
 export default function AddColorForm({ onNewColor = f => f }) {
-    const txtTitle = useRef();
-    const hexInput = useRef();
+    const [titleProps, resetTitle] = useInput("");
+    const [colorProps, resetColor] = useInput("#000000");
 
     const submit = e => {
         e.preventDefault();
-        onNewColor(txtTitle.current.value, hexInput.current.value);
-        txtTitle.current.value = ""; 
-        hexInput.current.value = "";        
+        onNewColor(titleProps.value, colorProps.value);
+        resetTitle();
+        resetColor();
     }
 
     return (
         <form onSubmit={submit}>
-            <input ref={txtTitle} type="text" placeholder="color title..." required />
-            <input ref={hexInput} type="color" required />
+            <input {...titleProps} type="text" placeholder="color title..." required />
+            <input  {...colorProps} type="color" required />
             <button type="submit">add</button>
         </form>
     )
 }
+
+// // imperative code, uncontrolled component
+// export default function AddColorForm({ onNewColor = f => f }) {
+//     const txtTitle = useRef();
+//     const hexInput = useRef();
+
+//     const submit = e => {
+//         e.preventDefault();
+//         onNewColor(txtTitle.current.value, hexInput.current.value);
+//         txtTitle.current.value = ""; 
+//         hexInput.current.value = "";        
+//     }
+
+//     return (
+//         <form onSubmit={submit}>
+//             <input ref={txtTitle} type="text" placeholder="color title..." required />
+//             <input ref={hexInput} type="color" required />
+//             <button type="submit">add</button>
+//         </form>
+//     )
+// }
