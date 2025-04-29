d8f150bffd75575b4492f230cb35a3c4797be51c
Elias <eliasnemr@gmail.com>
2025-04-29 08:37:39 +0300

    added color-list, onRate and onRemove event handlers

---
+++ color-organizer/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./components/App";
+// Get the root element
+const rootElement = document.getElementById("root");
+
+// Create a root
+const root = createRoot(rootElement);
+
+// Render your app
+root.render(<App />);
