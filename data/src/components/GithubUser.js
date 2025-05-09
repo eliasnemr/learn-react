369382ba07c20233353201039d71e7d21678d956
Elias <eliasnemr@gmail.com>
2025-05-09 11:50:54 +0300

    Creating a re-usable Fetch component that hides away re-usable fetch api code and gives default values for loading and error screens so as to render the same through out all the application

--- data/src/components/GithubUser.js
+++ data/src/components/GithubUser.js
@@ -1,29 +1,31 @@
 import React from "react";
 import { useFetch } from "../hooks/useFetch";
+import Fetch from "./Fetch";
+
 
 export default function GithubUser({login}) {    
-    const { loading, data, error } = useFetch(
-        `https://api.github.com/users/${login}`
-    );
-
-    if (loading) return <h1>loading...</h1>;
-    if (error) {
-        return <pre>{JSON.stringify(error, null, 2)}</pre>;
-    }
-    
-    const { avatar_url, login, name, location } = data;
-
-    return (<div className="githubUser">
-        <img
-            src={avatar_url}
-            alt={login}
-            style={{ width: 200 }}
+   return (
+        <Fetch 
+            uri={`https://api/github.com/${login}`}
+            renderSuccess={UserDetails}
         />
-        <div>
-            <h1>{data.login}</h1>
-            {name && <p>{name}</p>}
-            {location && <p>{location}</p>}
-        </div>
-    </div>)
+    )
 }
 
+function UserDetails({ data }) {
+    const { avatar_url, login, name, location } = data;
+
+    return (
+        <div className="githubUser">
+            <img
+                src={avatar_url}
+                alt={login}
+                style={{ width: 200 }}
+            />
+            <div>
+                <h1>{data.login}</h1>
+                {name && <p>{name}</p>}
+                {location && <p>{location}</p>}
+            </div>
+        </div>)
+}
