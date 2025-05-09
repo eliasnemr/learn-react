caca652df0536e88322c7de9a06268c56431abdb
Elias <eliasnemr@gmail.com>
2025-05-09 11:39:53 +0300

    Created a useFetch hook that'll make the fetch API logic re-usable across the app

--- data/src/components/GithubUser.js
+++ data/src/components/GithubUser.js
@@ -1,39 +1,29 @@
-import React, { useEffect, useState } from "react";
-
-const loadJSON = key => key && JSON.parse(localStorage.getItem(key));
-const saveJSON = (key, data) => localStorage.setItem(key, JSON.stringify(data));
+import React from "react";
+import { useFetch } from "../hooks/useFetch";
 
 export default function GithubUser({login}) {    
-    const [data, setUserData] = useState(loadJSON(`user:${login}`));
+    const { loading, data, error } = useFetch(
+        `https://api.github.com/users/${login}`
+    );
 
-
-    useEffect(() => {
-        if (!data) return;
-        if (data.login === login) return;
-
-        const { name, avatar_url, location } = data;
-        saveJSON(`user:${login}`, {
-            name,
-            login,
-            avatar_url,
-            location
-        });
-    }, [data]);
-
-    useEffect(() => {
-        if (!login) return;
-        if (data && data.login === login) return;
-
-        fetch(`https://api.github.com/users/${login}`)
-            .then(response => response.json())
-            .then(setUserData)
-            .catch(console.error);
-    }, [login]);
-
-    if (data) {
-        return <pre>{JSON.stringify(data, null, 2)}</pre>
+    if (loading) return <h1>loading...</h1>;
+    if (error) {
+        return <pre>{JSON.stringify(error, null, 2)}</pre>;
     }
+    
+    const { avatar_url, login, name, location } = data;
 
-    return <div>Not logged in yet!</div>
+    return (<div className="githubUser">
+        <img
+            src={avatar_url}
+            alt={login}
+            style={{ width: 200 }}
+        />
+        <div>
+            <h1>{data.login}</h1>
+            {name && <p>{name}</p>}
+            {location && <p>{location}</p>}
+        </div>
+    </div>)
 }
 
